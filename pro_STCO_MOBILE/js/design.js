bff0ce88b37285ccfab4d59848f60f698c272752
dumicode-hj103 <123072569+dumicode-hj103@users.noreply.github.com>
2024-04-10 22:02:17 +0800

    update

--- pro_STCO_MOBILE/js/design.js
+++ pro_STCO_MOBILE/js/design.js
@@ -1,9 +1,39 @@
 $(function () {
-  // 창 스크롤 이벤트를 감지하여 탭을 고정합니다.
+
+  var scrollDirection = {
+    UP: 'up',
+    DOWN: 'down'
+  };
+
+  var lastScrollTop = 0;
+  var currentDirection = null;
+
+  $(window).scroll(function () {
+    var currentScroll = $(this).scrollTop();
+
+    if (currentScroll > lastScrollTop) {
+      // Down
+      currentDirection = scrollDirection.DOWN;
+    } else {
+      // Up
+      currentDirection = scrollDirection.UP;
+    }
+
+    lastScrollTop = currentScroll;
+  });
+
+
   $(window).scroll(function () {
     var scrollTop = $(window).scrollTop();
-    var headerHeight = $('.gnb_nav2').outerHeight() + $('.stco_header').outerHeight();
     var tabPosY = $('.prodDetailTabBox .tabTitBox').offset().top;
+    var headerHeight = 0;
+
+    if (currentDirection == scrollDirection.UP) {
+      headerHeight = $('.stco_header').outerHeight() + $('.gnb_nav2').outerHeight()
+    } else if (currentDirection == scrollDirection.DOWN) {
+      headerHeight = $('.gnb_nav2').outerHeight()
+    }
+
     if (scrollTop >= tabPosY - headerHeight) {
       $('body').addClass('fixedTab');
     } else {
@@ -11,52 +41,82 @@ $(function () {
     }
   });
 
-  // 창 스크롤 이벤트를 감지하여 보이는 tabCont 영역을 활성화합니다.
   $(window).scroll(function () {
-    // 헤더의 고정된 영역의 높이를 계산합니다.
-    var headerHeight = $('.header').outerHeight();
+
+    var headerHeight = 0;
+
+    if (currentDirection == scrollDirection.UP) {
+      headerHeight = $('.tabTitBox').height() + $('.stco_header').outerHeight() + $('.gnb_nav2').outerHeight()
+    } else if (currentDirection == scrollDirection.DOWN) {
+      headerHeight = $('.tabTitBox').height() + $('.gnb_nav2').outerHeight()
+    }
     var topOfScreen = $(window).scrollTop() + headerHeight;
     var bottomOfScreen = topOfScreen + $(window).height();
 
-    // 각 .tabCont 요소를 반복합니다.
     $('.tabCont').each(function () {
       var $tabCont = $(this);
       var topOfElement = $tabCont.offset().top;
       var bottomOfElement = topOfElement + $tabCont.outerHeight();
 
-      // 뷰포트 내에 있는지 확인합니다.
       var inViewport = (bottomOfScreen > topOfElement) && (topOfScreen < bottomOfElement);
 
       if (inViewport) {
-        // 현재 .tabCont의 ID를 가져옵니다.
         var tabId = $tabCont.attr('id');
-
-        // 모든 버튼의 'on' 클래스를 제거하고 해당 버튼에 추가합니다.
         $('.btnTab').removeClass('on');
         $('.btnTab[data-tab="' + tabId + '"]').addClass('on');
-
-        // 반복문을 종료하고 뷰포트 내에서 첫 번째 tabCont만 활성화합니다.
         return false;
       }
     });
   });
 
-  // 버튼을 클릭하면 해당 tabCont로 애니메이션 스크롤됩니다.
-  $('.btnTab').on('click', function (e) {
-    e.preventDefault(); // 기본 링크 동작을 막습니다.
 
-    // 스크롤 할 대상 tabCont의 ID를 가져옵니다.
+
+  $('.btnTab').on('click', function (e) {
+    e.preventDefault();
+
     var targetTabId = $(this).data('tab');
 
-    // 대상 tabCont의 위치를 계산합니다.
-    var targetOffset = $('#' + targetTabId).offset().top + 10;
+    if (targetTabId) {
 
-    // 대상 tabCont로 애니메이션 스크롤합니다.
-    $('html, body').animate({
-      scrollTop: targetOffset
-    }, 300); // 애니메이션 스크롤 속도를 조절할 수 있습니다. 단위는 밀리초입니다.
+      var targetOffset = $('#' + targetTabId).offset().top;
+      var buttonOffset = $(this).offset().top;
+
+      var headerHeight = 0;
+      if (targetOffset < buttonOffset) {
+        headerHeight = $('.tabTitBox').height() + $('.stco_header').outerHeight() + $('.gnb_nav2').outerHeight()
+      } else {
+        headerHeight = $('.tabTitBox').height() + $('.gnb_nav2').outerHeight()
+      }
+
+      targetOffset -= headerHeight - 2;
+
+      $('html, body').animate({
+        scrollTop: targetOffset
+      });
+    }
   });
+
+
 });
+
+function targetClick(obj,target) {
+
+  var targetOffset = $('#' + target).offset().top;
+  var buttonOffset = $(obj).offset().top;
+
+  var headerHeight = 0;
+  if (targetOffset < buttonOffset) {
+    headerHeight = $('.tabTitBox').height() + $('.stco_header').outerHeight() + $('.gnb_nav2').outerHeight()
+  } else {
+    headerHeight = $('.tabTitBox').height() + $('.gnb_nav2').outerHeight()
+  }
+
+  targetOffset -= headerHeight - 2;
+
+  $('html, body').animate({
+    scrollTop: targetOffset
+  });
+}
 
 // 팝업을 엽니다.
 function openPop(id) {
