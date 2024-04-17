4140684318f800435eed89c7f560d8c8dda65dc2
dumicode-hj103 <123072569+dumicode-hj103@users.noreply.github.com>
2024-04-17 20:06:14 +0800

    20240417-3

--- pro_STCO_PC/js/design.js
+++ pro_STCO_PC/js/design.js
@@ -11,16 +11,41 @@ $(function () {
   });
 
   $(window).trigger('mousewheel');
+
+  // $('.btnTab').on('click', function (e) {
+  //   e.preventDefault();
+
+  //   var targetTabId = $(this).data('tab');
+
+  //   if (targetTabId) {
+
+  //     var targetOffset = $('#' + targetTabId).offset().top;
+  //     var buttonOffset = $(this).offset().top;
+
+  //     var headerHeight = 0;
+  //     if (targetOffset < buttonOffset) {
+  //       headerHeight = $('.tabTitBox').height() + $('.header').outerHeight();
+  //     } else {
+  //       headerHeight = $('.tabTitBox').height();
+  //     }
+
+  //     targetOffset -= headerHeight - 2;
+
+  //     $('html, body').animate({
+  //       scrollTop: targetOffset
+  //     });
+  //   }
+  // });
 });
 
 
 function targetClick(obj, target) {
 
   $('.tabTitBox .tabTit .btnTab:eq(0)').trigger('click');
-  
+
   var targetOffset = $('#' + target).offset().top;
 
-  var headerHeight = headerHeight = $('.header').height() + $('.tabTitBox').outerHeight() 
+  var headerHeight = headerHeight = $('.header').height() + $('.tabTitBox').outerHeight()
   targetOffset -= headerHeight - 2;
 
   $('html, body').animate({
